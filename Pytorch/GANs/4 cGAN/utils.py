24ccf99fb776c4ab65c27dc6892a3a8d9c4a7b23
Herbert Mühlburger <mail@muehlburger.at>
2021-12-10 08:22:45 +0100

    refactor name

---
+++ Pytorch/GANs/4 cGAN/utils.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+
+def gradient_penalty(critic, labels, real, fake, device="cpu"):
+    BATCH_SIZE, C, H, W = real.shape
+    epsilon = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    interpolated_images = real * epsilon + fake * (1 - epsilon)
+
+    # calculate critic scores
+    mixed_scores = critic(interpolated_images, labels)
+
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+
+    return gradient_penalty
+
+
+def save_checkpoint(state, filename="wgan_gp.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+
+def load_checkpoint(checkpoint, gen, critic):
+    print("=> Loading checkpoint")
+    gen.load_state_dict(checkpoint['gen'])
+    critic.load_state_dict(checkpoint['critic'])
