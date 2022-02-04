df5a34e959219ca88f582b5853821583e80fab41
Herbert Mühlburger <mail@muehlburger.at>
2022-02-04 09:29:15 +0100

    prepare data

--- Pytorch/RNNs/rnn.py
+++ Pytorch/RNNs/rnn.py
@@ -19,7 +19,7 @@ def unicodeToAscii(s):
         c for c in unicodedata.normalize('NFD', s)
         if unicodedata.category(c) != 'Mn'
         and c in all_letters
-    )
+        )
 
 print(unicodeToAscii('Ślusàrski'))
 
@@ -33,5 +33,11 @@ def readLines(filename):
     return [unicodeToAscii(line) for line in lines]
 
 for filename in findFiles('data/names/*.txt'):
-    category = os.path.splittext(os.path.basename(filename))[0]
-    all_categories.append()
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+print(category_lines['German'][:5])
